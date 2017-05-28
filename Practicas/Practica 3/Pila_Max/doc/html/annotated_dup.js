06d90b498cbd39725238870a54db6f48001db98c
mikel00per <williams@correo.ugr.es>
2017-05-28 18:36:05 +0200

    Borrado

--- Practicas/Practica 3/Pila_Max/doc/html/annotated_dup.js
+++
@@ -1,6 +0,0 @@
-var annotated_dup =
-[
-    [ "elemento", "structelemento.html", "structelemento" ],
-    [ "Pila_max", "classPila__max.html", "classPila__max" ],
-    [ "Vector_Dinamico", "classVector__Dinamico.html", "classVector__Dinamico" ]
-];
--- Practicas/Practica 3/Pila_Max/doc/html/structelemento.js
+++
@@ -1,6 +0,0 @@
-var structelemento =
-[
-    [ "operator<<", "structelemento.html#ad286bdbd3fbbc3639c5870ac7141513b", null ],
-    [ "max", "structelemento.html#ad788dc2f06ae38ba1c1a3fbe5464443d", null ],
-    [ "n", "structelemento.html#a0db819125f29dfafa6df3f899e5e0c81", null ]
-];
